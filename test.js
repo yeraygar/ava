53844696e2c9e5a027e0474d8a099e405feb7ab6
repositorioinformatico <jdgarcia@iesperemaria.com>
2022-11-24 19:13:43 +0100

    Ultimos test

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
 import test from 'ava';
 
 test('foo', t => {
-	t.is(2 + 2, 5);
+	t.is(2 + 2, 4);
 });
+
+function sum (a, b) {
+  return a + b;
+}
+
+test('foo is being tested', t => {
+  t.pass();
+})
+test('sum of 2 numbers', t => {
+  t.is(sum(1, 2), 3);
+})
+
